ab022a9e691ebc56ab8c78959609f396d125cce6
AprilWithoutYou <Dangj98@gmail.com>
2023-04-14 06:58:04 -0600

    Cambios componentes

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../../components/NavBar/Navbar";
 
 export default function Cart(){
     return(
-        <div>
+        <div className='container-xl bg-success'>
+        <Navbar/>
             <h1>Esta es la ruta Cart</h1>
         </div>
     )
