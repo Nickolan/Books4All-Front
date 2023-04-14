ecd018c9830187d16f9dd9f25186dd133678bab9
AprilWithoutYou <Dangj98@gmail.com>
2023-04-14 06:55:42 -0600

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export default function Footer(){
+    return(
+        <footer className="mt-5 bg-dark fixed-bottom">
+            <div className="container-fluid">
+                <div className="row border-top justify-content-between p-3">
+                    <div className="col-sm p-0">
+                    <Link to="/about" className="text-light text-decoration-none">About us</Link>
+                    </div>
+                    <div className="col-sm d-flex flex-row-reverse">
+                     <span className="text-light alig-items-end">©Book4all</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
