3c66f2c622a33157e755fc30baf660a061958758
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-04-15 07:58:10 -0300

    ReviewForm as component - BookDetail updated - createFormRoute eliminated

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Events from "./views/Events/Events"
 import Cart from './views/Cart/Cart';
 import Profile from './views/Profile/Profile';
 import {BookDetail} from './views/BookDetail/BookDetail';
-import ReviewFormPage from './views/ReviewFormPage/ReviewFormPage';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -20,7 +18,6 @@ function App() {
       <Route path='/about' element={<About/>}/>
       <Route path='/books' element={<Books/>}/>
       <Route path='/bookDetail/:bookId' element={<BookDetail/>}/>
-      <Route path='/createReview' element={<ReviewFormPage/>}/>
       <Route path='/events' element={<Events/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/profile' element={<Profile/>}/>
