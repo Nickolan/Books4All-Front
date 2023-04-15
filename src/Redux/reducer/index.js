48023b0c133507682eca7e6fd994323fec616182
Andres0210 <ing_amilkar@hotmail.com>
2023-04-15 14:29:05 -0500

    actualizacion de filtros combinados y searchbar

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -1,80 +1,97 @@
 import {
-  FILTER_BY_AUTHOR,
-  FILTER_BY_CATEGORY,
-  GET_BOOKS,
-  GET_BOOK_DETAIL,
-  ALPHABETICAL_ORDER,
-  RESET_FILTERS,
+    FILTER_BY_AUTHOR,
+    FILTER_BY_CATEGORY,
+    GET_BOOKS,
+    GET_BOOK_DETAIL,
+    ALPHABETICAL_ORDER,
+    RESET_FILTERS,
 } from "../actions/index";
 
 const initialState = {
-  books: [],
-  allBooks: [],
-  reviews: [],
-  bookDetail: [],
+    books: [],
+    allBooks: [],
+    reviews: [],
+    bookDetail: [],
+    filters: {
+        category: 'all',
+        author: 'all'
+    }
 };
 
+const filtrarLibros = (libros, genero, autor) => {
+    return libros.filter(libro => {
+        if (genero === 'all' && autor === 'all') {
+            return true;
+        } else if (genero === 'all' && autor !== 'all') {
+            return libro.authors?.includes(autor);
+        } else if (genero !== 'all' && autor === 'all') {
+            return libro.categories?.includes(genero);
+        } else {
+            return libro.categories?.includes(genero) && libro.authors?.includes(autor);
+        }
+    })
+}
+
 const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case GET_BOOKS:
-      return {
-        ...state,
-        books: action.payload,
-        allBooks: action.payload,
-      };
-    case "GET_NAME_BOOKS":
-      return {
-        ...state,
-        books: action.payload,
-      };
-    case GET_BOOK_DETAIL:
-      return {
-        ...state,
-        bookDetail: action.payload,
-      };
+    switch (action.type) {
+        case GET_BOOKS:
+            return {
+                ...state,
+                books: action.payload,
+                allBooks: action.payload,
+            };
+        case "GET_NAME_BOOKS":
+            return {
+                ...state,
+                books: action.payload,
+                allBooks: action.payload,
+            };
+        case GET_BOOK_DETAIL:
+            return {
+                ...state,
+                bookDetail: action.payload,
+            };
 
-    //    case POST_REVIEW:
-    //        return{
-    //            ...state,
-    //        }
-    case FILTER_BY_CATEGORY:
-      let array = [];
+        //    case POST_REVIEW:
+        //        return{
+        //            ...state,
+        //        }
 
-      state.allBooks.forEach((book) => {
-        book.categories && array.push(book);
-      });
+        case FILTER_BY_CATEGORY:
+            return {
+                ...state,
+                books: filtrarLibros(state.allBooks, action.payload, state.filters.author),
+                filters: { ...state.filters, category: action.payload }
+            }
 
-      return {
-        ...state,
-        books: array.filter((el) => el.categories[0] === action.payload),
-      };
-    case FILTER_BY_AUTHOR:
-      return {
-        ...state,
-        books: state.books.filter((book) =>
-          book.authors?.includes(action.payload)
-        ),
-      };
-    case ALPHABETICAL_ORDER:
-      return {
-        ...state,
-        books: [...state.books].sort((a, b) =>
-          action.payload === "ascendente"
-            ? a.title.localeCompare(b.title)
-            : b.title.localeCompare(a.title)
-        ),
-      };
-    case RESET_FILTERS:
-      return {
-        ...state,
-        books: state.allBooks,
-      };
+        case FILTER_BY_AUTHOR:
+            return {
+                ...state,
+                books: filtrarLibros(state.allBooks, state.filters.category, action.payload),
 
-    default:
-      return {
-        ...state,
-      };
-  }
+                filters: { ...state.filters, author: action.payload }
+            }
+
+        case ALPHABETICAL_ORDER:
+            return {
+                ...state,
+                books: [...state.books].sort((a, b) =>
+                    action.payload === "ascendente"
+                        ? a.title.localeCompare(b.title)
+                        : b.title.localeCompare(a.title)
+                ),
+            };
+        case RESET_FILTERS:
+            return {
+                ...state,
+                books: state.allBooks,
+            };
+
+        default:
+            return {
+                ...state,
+            };
+    }
 };
 
 export default rootReducer;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-function Card({name, author, image, categories, bookId}) {
+import noImage from '../../images/icon-image-not-found.webp'
+
+function Card({ name, author, image, categories, bookId }) {
     const dispatch = useDispatch();
     const [isFav, setIsFav] = useState(false)
     // add useSelector in initialState with myFavorites
@@ -15,19 +17,49 @@ function Card({name, author, image, categories, bookId}) {
     }
 
     return (
-        <div class="border border-dark d-flex col bg-secondary">
-            {/* {isFav ? (
-                <button class="d-block w-1 justify-content-end align-items-start" onClick={handleFavorites}>❤️</button>
-            ) : (
-                <button class="d-block w-1 align-items-start justify-content-end" onClick={handleFavorites}>🤍</button>
-            )} */}
-            <Link to={`/bookDetail/${bookId}`}>
-                <img class="w-100 p-3" src={image} alt="Max-width 40%" />
+
+        <div class="m-3 d-inline-block shadow-lg " style={{ width: "180px", height: "350px", padding: "10px" }}>
+            <Link style={{ color: "black", textDecoration: "none" }} to={`/bookDetail/${bookId}`}>
+                {/* {isFav ? (
+            <button class="d-block w-1 justify-content-end align-items-start" onClick={handleFavorites}>❤️</button>
+        ) : (
+            <button class="d-block w-1 align-items-start justify-content-end" onClick={handleFavorites}>🤍</button>
+        )} */}
+                <div class="d-flex flex-column">
+
+                    <div class="bg-light" style={{ width: "140px", height: "200px", margin: "0 auto", backgroundColor: "black" }}>
+                        {image ? <img style={{ width: "140px", height: "200px" }} src={image} alt="book" />
+                            : <img style={{ width: "140px", height: "200px" }} src={noImage} alt="not found" />
+                        }
+                    </div>
+
+                    <h5 style={{
+                        marginTop: "10px",
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: 2,
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        fontSize: "14px",
+
+                    }}>
+                        {name}
+                    </h5>
+                    <div style={{ display: "flex", flexDirection: "column", }}>
+                        <p style={{
+                            fontSize: "13px",
+                            marginTop: "10px",
+                            display: "-webkit-box",
+                            WebkitBoxOrient: "vertical",
+                            WebkitLineClamp: 2,
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+
+                        }} class="text-xs ">{author}</p>
+                        <p style={{ fontSize: "13px", color: "#088000" }}><small>{categories}</small></p>
+                    </div>
+                </div>
             </Link>
-                <h2>Title: {name}</h2>
-                <h2>Author: {author}</h2>
-                <h2>Categories: {categories}</h2>
-                
         </div>
     )
 }
