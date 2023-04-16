f0df04e0d1f6bf50d7d6ccb58ae8d1f876051c0f
Roberto Ochoa <oc_005@hotmail.com>
2023-04-15 21:26:02 -0300

    agrego link para ver el detalle de cada book al carrousel

--- src/components/Carrousel3/CarruselNews.js
+++ src/components/Carrousel3/CarruselNews.js
@@ -4,14 +4,8 @@ import "./Carrusel.css";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { getImages } from "../../Redux/actions/index";
-
-/* 
-const images = [
-    { url: 'https://maldon.es/wp-content/uploads/2016/04/12428422.jpg', title: 'Image 1' },
-    { url: 'https://www.sopitas.com/site/wp-content/uploads/2015/04/libro_naranja_mecanica1.jpg', title: 'Image 2' },
-    { url: 'https://marketplace.canva.com/EAFI171fL0M/1/0/1003w/canva-portada-de-libro-de-novela-ilustrado-color-azul-aqua-PQeWaiiK0aA.jpg', title: 'Image 3' },
-]
- */
+import img_default from "../Carrousel1/img/img_default.jpg";
+import { Link } from "react-router-dom";
 
 export default function Carrusel3(props) {
   const slides = [1];
@@ -24,8 +18,9 @@ export default function Carrusel3(props) {
     console.log(images);
   }, []);
 
-  const images = books.map((book) => book.image).slice(70, 100); //me devuelve un array de las primeras 10 imágenes
-  const bookTitle = books.map((book) => book.title).slice(70, 100);
+  const images = books.map((book) => book.image).slice(140, 160); //me devuelve un array de las primeras 10 imágenes
+  const bookTitle = books.map((book) => book.title).slice(140, 160);
+  const bookId = books.map((book) => book.id).slice(140, 160);
 
   const slideLeft = () => {
     var slider = document.getElementById("slider3");
@@ -50,8 +45,15 @@ export default function Carrusel3(props) {
           {slides.map((slide, index) => {
             return (
               <div className="slider-card ">
-                {images.map((img) => (
-                  <img className="slider-card w-50" src={img} alt={bookTitle} />
+                {images.map((img, index) => (
+                  <Link to={`/bookDetail/${bookId[index]}`}>
+                    <img
+                      className="slider-card w-50"
+                      src={img || img_default}
+                      alt={bookTitle[index]}
+                      key={index}
+                    />
+                  </Link>
                 ))}
               </div>
             );
