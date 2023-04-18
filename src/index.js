37e1cedbb0205f8f433fe10f42c02ea70d899564
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-04-18 09:44:35 -0300

    Axios Interceptors - first proposal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import { AxiosInterceptor } from './components/interceptors/axios.interceptor';
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import './index.css';
@@ -8,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider} from 'react-redux';
 import store from './Redux/store';
 
-// AxiosInterceptor();
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
