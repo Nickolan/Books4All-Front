4ff4b3935250dcd7e40517480ff5d6e5775c5746
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-04-21 18:43:01 -0300

    prueba

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { Provider} from 'react-redux';
 import store from './Redux/store';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = 'dev-vcimk1mq442q83wq.us.auth0.com';
+const clientId = 'Jb0tkNGAsOvygZViD8G1VGhcM5T8FRe3';
 
 
 const root = createRoot(document.getElementById("root"));
