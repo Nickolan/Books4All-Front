eaa219a44c0b275da7b170bbeccd9bb0a328e3d4
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-04-21 08:04:37 -0300

    token auth verification

--- src/components/PayButton/PayButton.jsx
+++ src/components/PayButton/PayButton.jsx
@@ -1,14 +1,22 @@
 import axios from "axios";
 // import { useSelector } from "react-redux";
 import { instance, url } from "../services/api";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 
 export const PayButton = ({cart}) =>{
     // const user = useSelector((state)=> state.auth)
+    const navigate= useNavigate();
+    const { user, isAuthenticated } = useAuth0();
+
+
 
     const handleCheckout = () =>{
+
         console.log(cart);
-            axios.post(`${url}/create-checkout-session`,{
+            instance.post(`${url}/create-checkout-session`,{
                 cart,
                 // userId: user_id,
             }).then((res)=>{
@@ -17,7 +25,19 @@ export const PayButton = ({cart}) =>{
                 }
             }).catch((err)=> console.log(err.message))
     }
+
+    const handleUnauthorizedUser = ()=>{
+        return(
+            <div class="alert alert-warning" role="alert">
+            "Please Sign In to buy"
+            </div>
+        )
+    }
+
     return(
-        <button onClick={()=> handleCheckout()}>Checkout</button>
+        user?
+        <button type = "button" className = "btn btn-primary" onClick={handleCheckout}>Checkout</button>
+        :
+        <button type = "button" className = "btn btn-primary disabled" onClick={handleUnauthorizedUser}>Please Sign In to Buy</button>
     )
 }
