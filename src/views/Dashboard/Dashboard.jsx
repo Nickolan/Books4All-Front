291352c37e3723f7b8b42776000a6afbda3c19c6
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-04-24 11:39:06 -0300

    dashboard auth

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,8 +1,19 @@
 import Footer from '../../components/Footer/Footer'
 import Navbar from '../../components/NavBar/Navbar'
 import img from './images/Manufacturing-KPI-Dashboard-133807082'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export const Dashboard=()=>{
+    const navitgate = useNavigate()
+    const role = useSelector(state => state.role)
+    useEffect(() => {
+        if (role.name !== 'admin') {
+            navitgate('/home')
+        }
+    }, [])
+
     return(
         <>
         <Navbar/>
