773dc4fd749169d9ea60f8e880535ad8195d017a
mariano588 <marianocaroso@hotmail.com>
2023-04-27 10:42:33 -0300

    busqueda actualizada

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { Sidebar } from "./components/Sidebar/Sidebar";
 import { getBooks, getUserFromDb, getUsers, getDeletedBooks } from "./Redux/actions";
 import { PostUser } from "./components/PostUser/PostUser";
 import { useAuth0 } from "@auth0/auth0-react";
-// axios.defaults.baseURL ="https://books4all-back-production-bd65.up.railway.app/";
+//axios.defaults.baseURL ="https://books4all-back-production-bd65.up.railway.app/";
 axios.defaults.baseURL = "http://localhost:3001/";
 
 
