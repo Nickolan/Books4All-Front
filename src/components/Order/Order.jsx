75e412ecee81c3249a8204ae4386f446dbcaf63d
Roberto Ochoa <oc_005@hotmail.com>
2023-04-28 17:18:16 -0300

    estilos carrito

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -29,8 +29,11 @@ const Order = ({ setCurrentPage }) => {
         <div>
             <div class='d-flex mt-1 justify-content-center'>
                 <span class='fw-bold mx-2'>ORDER BY NAME: </span>
+                <div >
                 <button class={`btn btn-sm btn-outline mx-1  ${activeOrder === "ascendente" ? "active" : ""}`} value="ascendente" onClick={(event) => handleClickAlph(event, "ascendente")} >A-Z</button>
                 <button class={`btn btn-sm btn-outline mx-1 ${activeOrder === "descendente" ? "active" : ""}`} value="descendente" onClick={(event) => handleClickAlph(event, "descendente")} >Z-A</button>
+
+                </div>
                 {/* <button class='btn btn-transparent text-light'>Rating</button> */}
                 {/* <button class='btn btn-transparent text-light'>author A-Z</button> */}
                 <button class='btn btn-sm btn-outline-dark  bg-light' style={{marginLeft:'70px'}} onClick={handleReset}>RESET FILTERS</button>
