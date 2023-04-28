626fbfb83efd9b58022c7a72214b1d1e1e616e08
Roberto Ochoa <oc_005@hotmail.com>
2023-04-27 21:58:23 -0300

    corrección dark mode styles cart

--- src/views/Cart/Cart.jsx
+++ src/views/Cart/Cart.jsx
@@ -27,14 +27,14 @@ export default function Cart(){
 
     return(
         <>
-        <div className='container-xl'>
+        <div className='container-xl '>
         <Navbar/>
         <div className="mt-4 ">
         <Link to='/books' >
             <img class="BookDetail_backButton__i1i5t" src="https://res.cloudinary.com/dvldakcin/image/upload/v1681620387/Countries/back_lblp4n.png"/>
        </Link>
         </div>
-            <div className="flex-row mt-5 border rounded p-4 w-50 align-content-center">
+            <div className="flex-row mt-5 border rounded p-4 w-50 align-content-center ">
             {title.map((title,index)=><p key={index}>(1) {title} <button  type="button" class="btn " onClick={()=>handleClose(id[index])}>X</button></p>)} 
            <hr />
              <h6 className="p-2 w-25 mt-2">Total: U$S {price.length && price[0].reduce((a,b)=> Math.floor(Number(a)+Number(b)),0)}</h6>
