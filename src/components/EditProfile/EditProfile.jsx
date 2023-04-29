b7f05e8edc4b36e7cbec32aed35a4cf598da6c19
AprilWithoutYou <Dangj98@gmail.com>
2023-04-29 08:17:49 -0600

    cambios profile

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,12 +1,13 @@
-import { TextField, Box, Button, IconButton, Avatar, Container} from "@mui/material";
+import { TextField, Box, Button, IconButton, Avatar } from "@mui/material";
 import { useState } from "react";
 import { updateProfile } from "../services/updateProfile";
 import { validation } from "./Validation";
 import Widget from "../Widget/Widget";
 import { toast } from "react-toastify";
+import CloseIcon from '@mui/icons-material/Close';
 
 
-  const EditProfile = ({user}) => {
+  const EditProfile = ({user, handleClose}) => {
 
     const [updatedUser, setUpdatedUser] = useState({
         alterName: user.alterName,
@@ -44,13 +45,14 @@ import { toast } from "react-toastify";
         updateProfile(user.name, updatedUser).then(result => toast.success(result)).catch(error => toast.error(error.message))
       }
 
-    console.log(updatedUser)
-
-
 
     return(
-        <div style={{ width:'100%', height: '100%', backgroundColor:'black'}}>
         <Box display="flex" flexDirection="column" alignItems="center" style={{ backgroundColor: 'white', padding: '20px',  border: '1px solid #b2bec3', borderRadius: '20px'}}> 
+        <Box display="flex" justifyContent="flex-end" flexGrow="1" width="100%">
+        <IconButton color="inherit" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
     <form onSubmit={handleSubmit}>
         <Avatar src={updatedUser.picture} style={{ margin: 'auto', width: 70, height: 70}} alt="user picture"/>
         <Widget updatedUser={updatedUser} setUpdatedUser={setUpdatedUser}/>
@@ -58,9 +60,8 @@ import { toast } from "react-toastify";
         <TextField  margin="normal" variant='outlined' label='email' name="email" value={updatedUser.email} onChange={handleOnChange}/>
         <TextField margin="normal" multiline maxRows={4} minRows={4} variant='outlined' label='about' name="about" value={updatedUser.about} onChange={handleOnChange}/>
         <Button variant="outlined" type='submit'>Edit profile</Button>
-    </form>
+      </form>
         </Box>
-        </div>
     )
   };
   
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,9 +1,8 @@
-import LogoutButton from "../../components/LogoutButton/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserFromDb } from "../../Redux/actions";
-import { Box, Button, Container, Divider, Grid, Avatar} from "@mui/material";
+import { Button, Container, Divider, Grid, Avatar, Tab, Tabs} from "@mui/material";
 import Grow from '@mui/material/Grow';
 import EditProfile from "../../components/EditProfile/EditProfile";
 export default function Auth(){
@@ -15,46 +14,73 @@ export default function Auth(){
     const dbUser = useSelector(state=>state.dbUser);
 
     const [showEditForm, setShowEditForm] = useState(false);
+    
+    const [tabValue, setTabValue] = useState(0);
 
     useEffect( ()=> {
             dispatch(getUserFromDb(user?.nickname))
     },[])
 
-    const handleClick = () => {
-        setShowEditForm(!showEditForm)
+    const handleOpen = () => {
+        setShowEditForm(true)
     }
+
+    const handleClose = () => {
+      setShowEditForm(false);
+    };
 
 
         return(
-            <Container sx={{ display: 'flex', minHeight: '85vh'  }}>
+            <Container sx={{ display: 'flex', minHeight: '85.9vh'  }}>
             <Grid container sx={{ pt: 2}}>
             <Grid item xs={3} style={{ textAlign: 'center'}}>
 
             <Avatar src={dbUser.picture} style={{ margin: 'auto', width: 70, height: 70}} alt=""/>
 
-            <Button onClick={handleClick}>Edit</Button>
+            <Button onClick={handleOpen} style={{ color: 'black' }}>Edit</Button>
             
             <Grow in={showEditForm} style={{ position: 'absolute', top: '20%', left: '40%', maxWidth:'300px' }}>
             <div>
-              {showEditForm && <EditProfile user={dbUser} />}
+              {showEditForm && <EditProfile user={dbUser} handleClose={handleClose}/>}
             </div>
-          </Grow > 
+            </Grow > 
+            <Divider variant="middle" style={{ backgroundColor: 'black' }}/>
+             <h4>{dbUser.alterName}</h4>
+            <Divider variant="middle" style={{ backgroundColor: 'black' }}/>
+             <h4>{dbUser.email}</h4>
+           <Divider variant="middle" style={{ backgroundColor: 'black' }}/>
+             <h4>About</h4>
+             <h6>{dbUser.about}</h6>
             </Grid>
-            <Divider orientation="vertical" flexItem xs={1}/>
+            <Divider orientation="vertical" flexItem xs={1} style={{ backgroundColor: 'black' }}/>
             <Grid item xs={8} sx={{ pl: 1}}>
-            <Box sx={{ display: 'grid', gap: 12, gridTemplateColumns: 'repeat(2, 1fr)', mx: 2, textAlign: 'center'}}>
-                      <div>
-                         <h2>Recent Purchases</h2>
-                         </div>
-                         <div>
-                         <h2>Your Wishlist</h2>
-                         </div>
-                    </Box>
-                     <h1>Your recent reviews</h1>
-
+            <Tabs value={tabValue} onChange={(event, newValue) => setTabValue(newValue)}>
+            <Tab label="Purchases" />
+            <Tab label="Wishlist" />
+            <Tab label="Reviews" />
+            </Tabs>
+            <TabPanel value={tabValue} index={0}>
+              Aqui van las Purchases
+            </TabPanel>
+            <TabPanel value={tabValue} index={1}>
+              Aqui va la Wishlist
+            </TabPanel>
+            <TabPanel value={tabValue} index={2}>
+              Aqui van las Reviews
+            </TabPanel>
             </Grid>
         </Grid>
         </Container>
         )
-    
+}
+
+
+function TabPanel(props) {
+  const {children, value, index} = props
+
+  return(
+    <div>{
+      value===index && children
+      }</div>
+  )
 }
