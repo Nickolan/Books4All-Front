b8411f0446736f639fd2611a8b134e7c83878b6f
Roberto Ochoa <oc_005@hotmail.com>
2023-04-29 12:40:47 -0300

    agrego loader al about

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import Navbar from "../../components/NavBar/Navbar";
 import { integrantes } from "./datos_integrantes";
 import {AboutCard} from "../../components/AboutCard/AboutCard"
 import Footer from "../../components/Footer/Footer";
+import { Loader } from "../../components/Loader/Loader";
 
 
 export default function About(){
+  const[loader, setLoader]= useState(false)
+
+  useEffect(()=>{
+    setLoader(true)      
+    setTimeout(() => {
+       setLoader(false);
+     }, 150);
+  },[])
 
     return(
         <div className="container-xl  ">
           <Navbar />
+          {loader ?
+                 <div class="mx-auto d-flex justify-content-center" style={{ width: "80%", marginBottom: '40px',height:'600px', marginTop:'20%' }}>
+                 <Loader/>
+              </div>
+          :
+          <>         
           <div className="d-flex justify-content-center ">
             <h1>About Us</h1>
           </div>
@@ -28,6 +43,8 @@ export default function About(){
               })}
             </div>
           </div>
+          </>
+          }
           <Footer />
         </div>
       );
