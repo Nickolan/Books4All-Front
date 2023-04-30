a51ae2fe8bb3a275146aced385e72e5d5b051e40
AprilWithoutYou <Dangj98@gmail.com>
2023-04-29 20:53:56 -0600

    colores pestañas

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -2,11 +2,23 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserFromDb } from "../../Redux/actions";
-import { Button, Container, Divider, Grid, Avatar, Tab, Tabs} from "@mui/material";
+import { Button, Container, Divider, Grid, Avatar, Tab, Tabs, createMuiTheme, ThemeProvider} from "@mui/material";
 import Grow from '@mui/material/Grow';
 import EditProfile from "../../components/EditProfile/EditProfile";
 import ProfileBoughts from "./profileBoughts";
 import LogoutButton from "../../components/LogoutButton/LogoutButton";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000000', // Cambia el color primario a rojo
+    },
+    secondary: {
+      main: '#b2b2b2', // Cambia el color secundario a verde
+    },
+  },
+});
+
 
 
 export default function Auth(){
@@ -35,7 +47,8 @@ export default function Auth(){
 
 
         return(
-            <Container sx={{ display: 'flex', minHeight: '85.9vh'  }}>
+          <ThemeProvider theme={theme}>
+            <Container sx={{ display: 'flex', minHeight: '87.9vh'  }}>
             <Grid container sx={{ pt: 2}}>
             <Grid item xs={3} style={{ textAlign: 'center'}}>
 
@@ -58,8 +71,9 @@ export default function Auth(){
              <LogoutButton/>
             </Grid>
             <Divider orientation="vertical" flexItem xs={1} style={{ backgroundColor: 'black' }}/>
-            <Grid item xs={8} sx={{ pl: 1}}>
-            <Tabs value={tabValue} onChange={(event, newValue) => setTabValue(newValue)}>
+            <Grid item xs={8} sx={{ pl: 1, justifyContent: 'center' }}>
+            <Tabs value={tabValue} onChange={(event, newValue) => setTabValue(newValue)} sx={{
+              backgroundColor: 'transparent'}} indicatorColor="secondary">
             <Tab label="Purchases" />
             <Tab label="Wishlist" />
             <Tab label="Reviews" />
@@ -76,6 +90,7 @@ export default function Auth(){
             </Grid>
         </Grid>
         </Container>
+        </ThemeProvider>
         )
 }
 
