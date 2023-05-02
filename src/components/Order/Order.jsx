e048c625445300dee3b36fb45cfd0b6f39010a4f
Roberto Ochoa <oc_005@hotmail.com>
2023-05-02 15:32:05 -0300

    tooltip books

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -29,24 +29,24 @@ const Order = ({ setCurrentPage }) => {
     return (
         <div>
             <div class='d-flex mt-1 justify-content-center'>
-                <span class='fw-bold mx-2'>ORDER BY NAME: </span>
+                <span class='fw-bold mx-2' title='order by name'>ORDER BY NAME: </span>
                 <div>
                 {theme==='light'?
                 <div>
-                 <button class={`btn btn-sm btn-outline mx-1 text-black  ${activeOrder === "ascendente" ? "active" : ""}`} value="ascendente" onClick={(event) => handleClickAlph(event, "ascendente")} >A-Z</button> 
-                 <button class={`btn btn-sm btn-outline mx-1 text-black ${activeOrder === "descendente" ? "active" : ""}`} value="descendente" onClick={(event) => handleClickAlph(event, "descendente")} >Z-A</button>
+                 <button class={`btn btn-sm btn-outline mx-1 text-black  ${activeOrder === "ascendente" ? "active" : ""}`} value="ascendente" title='order by name A-Z' onClick={(event) => handleClickAlph(event, "ascendente")} >A-Z</button> 
+                 <button class={`btn btn-sm btn-outline mx-1 text-black ${activeOrder === "descendente" ? "active" : ""}`} value="descendente" title='order by name Z-A' onClick={(event) => handleClickAlph(event, "descendente")} >Z-A</button>
                  </div>
                 :
                 <div >
-                <button class={`btn btn-sm btn-outline mx-1 text-white  ${activeOrder === "ascendente" ? "active" : ""}`} value="ascendente" onClick={(event) => handleClickAlph(event, "ascendente")} >A-Z</button> 
-                 <button class={`btn btn-sm btn-outline mx-1 text-white ${activeOrder === "descendente" ? "active" : ""}`} value="descendente" onClick={(event) => handleClickAlph(event, "descendente")} >Z-A</button>
+                <button class={`btn btn-sm btn-outline mx-1 text-white  ${activeOrder === "ascendente" ? "active" : ""}`} value="ascendente" title='order by name A-Z' onClick={(event) => handleClickAlph(event, "ascendente")} >A-Z</button> 
+                 <button class={`btn btn-sm btn-outline mx-1 text-white ${activeOrder === "descendente" ? "active" : ""}`} value="descendente" title='order by name Z-A' onClick={(event) => handleClickAlph(event, "descendente")} >Z-A</button>
                  </div>
                 }    
 
                 </div>
             
                
-                <button class='btn btn-sm btn-outline-dark  bg-light' style={{marginLeft:'70px'}} onClick={handleReset}>RESET FILTERS</button>
+                <button class='btn btn-sm btn-outline-dark  bg-light' style={{marginLeft:'70px'}} onClick={handleReset} title='reset filters'>RESET FILTERS</button>
 
                 </div>
                 {/* <button class='btn btn-transparent text-light'>Rating</button> */}
