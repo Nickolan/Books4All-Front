1cc3b0ee233cc8e21ab26d5a7b5361cbbe28375a
Roberto Ochoa <oc_005@hotmail.com>
2023-05-02 15:17:01 -0300

    tooltip book detail

--- src/views/BookDetail/BookDetail.jsx
+++ src/views/BookDetail/BookDetail.jsx
@@ -149,7 +149,7 @@ const BookDetail = (props) => {
                                 <div className={style.allContentContainer}>
                                     <div className={style.imgContainer}>
                                         
-                                        <img className={style.bookImg} alt='Not found' src={el.image} width='350px' height='200px'></img>
+                                        <img title={el.title} className={style.bookImg} alt='Not found' src={el.image} width='350px' height='200px'></img>
                                     </div>
                                     <div className={style.contentContainer}>
                                         <h1 className={style.title}>{el.title}</h1>
@@ -166,7 +166,7 @@ const BookDetail = (props) => {
                                             <div className={style.container_price}>
                                                 <h3 className={style.price}>${el?.price}</h3>
                                             </div>
-                                            <button type="button" className="btn btn-dark mt-1 ms-3" onClick={handleClickAddCart}>Add to cart</button>
+                                            <button type="button" className="btn btn-dark mt-1 ms-3" onClick={handleClickAddCart} title='Add to cart'>Add to cart</button>
                                             <Typography sx={{ margin: '0 10px 0 10px', fontStyle: 'italic' }}>Avalaible units: {el.stock}</Typography>
                                         </div>
                                         <div>
@@ -231,7 +231,7 @@ const BookDetail = (props) => {
                                     <Box sx={{ marginTop: '30px' }}>
                                         <Typography>
                                             Did you buy this book? Share your opinion and
-                                            <Button sx={{}} onClick={handleShowReview}> leave a review!</Button>
+                                            <Button sx={{}} onClick={handleShowReview} title='leave a review'> leave a review!</Button>
 
                                         </Typography>
 
@@ -248,6 +248,7 @@ const BookDetail = (props) => {
                     showReview={showReview}
                     updateDetail={setUpdateReview}
                     updateReview={updateReview}
+                    
                 />}
             </Container>
           
