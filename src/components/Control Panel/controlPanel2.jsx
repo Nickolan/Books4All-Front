8dde1efbfb807fe8b0950ceed5df6729703b007e
Andres0210 <ing_amilkar@hotmail.com>
2023-05-02 18:18:31 -0500

    estilos

--- src/components/Control Panel/controlPanel2.jsx
+++ src/components/Control Panel/controlPanel2.jsx
@@ -1,21 +1,18 @@
 import { Divider, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import { Fragment } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+
 import PeopleIcon from '@mui/icons-material/People';
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import { grey, purple, red, yellow } from '@mui/material/colors';
+import { purple, red, yellow } from '@mui/material/colors';
 import CreateIcon from '@mui/icons-material/Create';
 
 export default function ControlPanel2({ setSection }) {
 
     return (
         <Fragment>
-
-
             <ListItemButton onClick={() => setSection('Dashboard')}>
                 <ListItemIcon>
                     <DashboardIcon color='primary' />
@@ -65,30 +62,6 @@ export default function ControlPanel2({ setSection }) {
                 <ListItemText primary="Create Book" />
             </ListItemButton>
             <Divider sx={{ margin: '5px 0 5px 0' }} />
-            {/* <div>
-                    <button onClick={() => setSection('Dashboard')} >DashBoard</button>
-                </div>
-                <div>
-                    <button onClick={() => setSection('Admin')} >Admin</button>
-                </div>
-                <div>
-                    <button onClick={() => setSection('Active Users')} >Active Users</button>
-                </div>
-                <div>
-                    <button onClick={() => setSection('Inactive Users')} >Inactive Users</button>
-                </div>
-                <div>
-                    <button onClick={() => setSection('Active Books')} >Active Books</button>
-                </div>
-                <div>
-                    <button onClick={() => setSection('Inactive Books')} >Inactive Books</button>
-                </div>
-                <div>
-                    <Link to={`/formCreateBook`}>
-                        <button class='btn btn-success'>Create Book</button>
-                    </Link>
-                </div> */}
-
         </Fragment>
     )
 }
