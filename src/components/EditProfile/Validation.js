0fd12127f92a6619e8c139058f550a1bc0bf927c
Roberto Ochoa <oc_005@hotmail.com>
2023-05-03 10:25:42 -0300

    condicional validation

--- src/components/EditProfile/Validation.js
+++ src/components/EditProfile/Validation.js
@@ -1,22 +1,19 @@
-
-const emailRegex =/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
 export const validation = (updatedUser) => {
+  let errors = {};
 
-    let errors = {};
-  
-    if (updatedUser.alterName.length > 20) {
-      errors.alterNme = "Name cannot exceed 20 characters";
-    }
-  
-    if (!emailRegex.test(updatedUser.email)) {
-      errors.email = "Invalid email";
-    }
-  
-    if (updatedUser.about.length > 110) {
-      errors.about = "about cannot exceed 110 chracters";
-    }
+  if (updatedUser.alterName.length > 20) {
+    errors.alterNme = "Name cannot exceed 20 characters";
+  }
 
-    return errors;
+  if (!emailRegex.test(updatedUser.email)) {
+    errors.email = "Invalid email";
+  }
 
-}
+  if (updatedUser.about?.length > 110) {
+    errors.about = "about cannot exceed 110 chracters";
+  }
+
+  return errors;
+};
